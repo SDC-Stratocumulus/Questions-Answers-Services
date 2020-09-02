185cf3e37aa5a24d94e3e81273a3c8c8769cd021
Deo-Mac <druplall@gmail.com>
2020-09-02 14:26:47 -0400

    created loader IO route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const router = require('../database-mysql/routes.js');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../loader/')));
+
 // Set up our routes
 app.use('/qa', router);
 
