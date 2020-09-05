bb01af0efaade6efb311d1cc8b5f7f9770f75bab
Deo-Mac <druplall@gmail.com>
2020-09-05 13:31:18 -0400

    Clean up

--- middleware/index.js
+++ middleware/index.js
@@ -6,16 +6,13 @@ module.exports = function () {
     if (req.method === 'GET') {
       //console.log(req.method);
       const productId = req.query.product_id;
-      console.log(productId);
       client.get(productId, (error, data) => {
         if (error) {
           console.log(error);
         }
         if (data !== null) {
-          console.log('Using Redis cache');
           res.send(JSON.parse(data));
         } else {
-          console.log('No data in redis cache');
           next();
         }
       });
